36cebb387f942ed48e9bb7e2520dcc886409b5ae
AyrtonR <AyrtonR@users.noreply.github.com>
2022-06-06 18:43:53 +0300

    starting over yep

--- app/src/main/java/com/example/MPProject/NewsModel.kt
+++ app/src/main/java/com/example/MPProject/NewsModel.kt
@@ -1,16 +1,3 @@
 package com.example.MPProject
 
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
 
-class NewsModel: ViewModel() {
-    var news = mutableStateOf(listOf<News>())
-
-
-    fun addNews(new: News){
-        var newNews = news.value.toMutableList()
-        newNews.add(new)
-        news.value = newNews
-    }
-
-}
